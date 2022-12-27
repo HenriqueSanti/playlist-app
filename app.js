1bc7c065907e93c54916f0e313dea2acd2e37757
HenriqueSanti <henriquesanti.dev@gmail.com>
2022-12-27 16:27:26 -0400

    Create: Register

--- app.js
+++ app.js
@@ -2,16 +2,32 @@ require("dotenv").config();
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const dbConnection = require("./databse/db");
+const path = require("path");
+const Music = require("./model/Music");
 
 mongoose.set("strictQuery", true);
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
+
 dbConnection();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("index");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/create", async (req, res) => {
+  const music = req.body;
+  await Music.create(music);
+  res.redirect("/");
 });
 
 app.listen(port, () =>
